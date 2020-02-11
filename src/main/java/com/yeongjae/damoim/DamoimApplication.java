5a1f1d34c4b9563bfc88105a10b6e5ecad34b9b5
jaeho214 <jaeho214@naver.com>
2020-02-11 20:49:39 +0900

    git init

---
+++ src/main/java/com/yeongjae/damoim/DamoimApplication.java
@@ -0,0 +1,13 @@
+package com.yeongjae.damoim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamoimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DamoimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yeongjae/damoim/DamoimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeongjae.damoim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamoimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
