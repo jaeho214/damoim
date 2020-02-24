17b327180edb53e3e0d7c691fdda033b13c89c7b
jaeho214 <jaeho214@naver.com>
2020-02-25 01:07:57 +0900

    add jwt

---
+++ src/main/java/com/yeongjae/damoim/global/jwt/JwtService.java
@@ -0,0 +1,63 @@
+package com.yeongjae.damoim.global.jwt;
+
+import com.yeongjae.damoim.global.error.exception.UserDefineException;
+import io.jsonwebtoken.*;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class JwtService {
+
+    private final String SECRET_KEY = "DAMOIM";
+    private final long EXPIRE_TIME = 100000 * 60 * 60;
+
+    public String createJwt(String email){
+        Map<String, Object> claimMap = new HashMap<>();
+        claimMap.put("EMAIL", email);
+
+        Date expireTime = new Date();
+        expireTime.setTime(expireTime.getTime() + EXPIRE_TIME);
+
+        return Jwts.builder()
+                .setHeaderParam("typ", "JWT")
+                .setHeaderParam("issueDate", System.currentTimeMillis())
+                .setClaims(claimMap)
+                .setExpiration(expireTime)
+                .signWith(SignatureAlgorithm.HS256, generateKey())
+                //직렬화
+                .compact();
+    }
+
+    private byte[] generateKey() {
+        try{
+            return SECRET_KEY.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new UserDefineException("키를 변환하는 데에 실패하였습니다.");
+        }
+    }
+
+    public boolean isUsable(String token) {
+        try {
+            Jws<Claims> claimsJws = Jwts.parser()
+                    .setSigningKey(generateKey())
+                    .parseClaimsJws(token);
+            return true;
+        }catch (ExpiredJwtException e){
+            return false;
+        }
+    }
+
+    public String findEmailByJwt(String token){
+        Claims claims = Jwts.parser()
+                .setSigningKey(generateKey())
+                .parseClaimsJws(token)
+                .getBody();
+
+        return (String) claims.get("EMAIL");
+    }
+
+}
