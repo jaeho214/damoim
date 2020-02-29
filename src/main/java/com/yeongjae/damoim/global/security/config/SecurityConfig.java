b3d6fbce03dafdc1d4ec1f6b7539337b8223a7d0
jaeho214 <jaeho214@naver.com>
2020-03-01 02:24:36 +0900

    add security filter

--- src/main/java/com/yeongjae/damoim/global/security/config/SecurityConfig.java
+++ src/main/java/com/yeongjae/damoim/global/security/config/SecurityConfig.java
@@ -1,23 +1,44 @@
 package com.yeongjae.damoim.global.security.config;
 
+import com.yeongjae.damoim.global.filter.AccessDeniedHandlerCustom;
+import com.yeongjae.damoim.global.filter.AuthenticationEntryPointCustom;
 import com.yeongjae.damoim.global.filter.JwtAuthenticationFilter;
 import com.yeongjae.damoim.global.jwt.JwtService;
+import com.yeongjae.damoim.global.security.service.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @RequiredArgsConstructor
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final JwtService jwtService;
+    private final AccessDeniedHandlerCustom accessDeniedHandlerCustom;
+    private final AuthenticationEntryPointCustom authenticationEntryPointCustom;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    private static final String[] AUTH_ARR = {
+            "/v2/api-docs",
+            "/configuration/ui",
+            "/swagger-resources/**",
+            "/configuration/security",
+            "/swagger-ui.html/**",
+            "/webjars/**",
+            "favicon.ico"
+    };
 
     @Bean
     @Override
@@ -33,17 +54,29 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                     .authorizeRequests()
-                        .antMatchers(HttpMethod.POST, "/*/sign/signIn").permitAll()
-                        .antMatchers(HttpMethod.POST, "/*/sign").permitAll()
-                        .antMatchers("/*/sign/pw", "/*/sign/check").permitAll()
+                        .antMatchers(HttpMethod.POST, "/damoim/sign/signIn").permitAll()
+                        .antMatchers(HttpMethod.POST, "/damoim/sign").permitAll()
                         .anyRequest().hasRole("USER")
                 .and()
-                    .addFilterBefore(new JwtAuthenticationFilter(jwtService), UsernamePasswordAuthenticationFilter.class);
+                    .exceptionHandling().accessDeniedHandler(accessDeniedHandlerCustom)
+                .and()
+                    .exceptionHandling().authenticationEntryPoint(authenticationEntryPointCustom)
+                .and()
+                    .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/v2/api-docs", "/swagger-resources/**",
-                "/swagger-ui.html", "/webjars/**", "/swagger/**");
+        web.ignoring()
+                .antMatchers(AUTH_ARR)
+                .antMatchers("/damoim/sign/pw")
+                .antMatchers("/damoim/sign/check/**");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder);
     }
 }
