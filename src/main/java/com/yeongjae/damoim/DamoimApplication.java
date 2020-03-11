0c3c3b3fe25c841c076f30175a1cef10ed4c1d0f
jaeho214 <jaeho214@naver.com>
2020-03-11 18:01:41 +0900

    update config for S3

--- src/main/java/com/yeongjae/damoim/DamoimApplication.java
+++ src/main/java/com/yeongjae/damoim/DamoimApplication.java
@@ -1,22 +1,23 @@
 package com.yeongjae.damoim;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class DamoimApplication {
 
-    public static final String APPLICATION_LOCATIONS =
-            "spring.config.location="
-            + "classpath:application.yml,"
-            + "classpath:aws.yml,"
-            + "classpath:secret.yml";
+//    public static final String APPLICATION_LOCATIONS =
+//            "spring.config.location="
+//            + "classpath:application.yml,"
+//            + "classpath:aws.yml,"
+//            + "classpath:secret.yml";
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(DamoimApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
-        //SpringApplication.run(DamoimApplication.class, args);
+//        new SpringApplicationBuilder(DamoimApplication.class)
+//                .properties(APPLICATION_LOCATIONS)
+//                .run(args);
+        SpringApplication.run(DamoimApplication.class, args);
     }
 
 }
